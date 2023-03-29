b866291ac2baceae6f73491743d593486f164b9e
Christian <chrbudtz@gmail.com>
2023-03-29 08:15:11 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {Button} from "sveltestrap";
+
+    let fetchData = async ()=>{
+        const res = await fetch("https://api.dataforsyningen.dk/adresser?vejnavn=Rødkildevej&husnr=46&struktur=mini");
+        const data = await res.json();
+        const text = JSON.stringify(data)
+        return text;
+    };
+
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button on:click={fetchData}>Test</Button>
+{#await fetchData}
+    {:then}
+
+{/await}
+
